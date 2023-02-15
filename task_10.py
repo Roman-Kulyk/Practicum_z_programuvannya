43f706bd7f856edb7b76263ed664956bb50bfcb9
Roman Kulyk <roman.kulyk@globallogic.com>
2023-02-15 21:35:57 +0200

    added new files task_2-20

---
+++ task_10.py
@@ -0,0 +1,4 @@
+#Вводиться ціле невід’ємне число n (n ≤ 100). Виведіть 2n.
+
+num = int(input('Enter a number: '))
+print(pow(2,num))
---
+++ task_3.py
@@ -0,0 +1,7 @@
+# Напишіть програму, яка отримує такі дані: ім’я, вік, хобі, введені з клавіатури (вводяться на окремих рядках),
+# і друкує на екрані одним повідомленням повну інформацію на основі введених даних.
+
+name =input('Enter your full name:')
+age = input('Enter your age:')
+hobby = input('Enter your hobby:')
+print(f'My name is  {name}.I am {age} and my hobby is {hobby}.')
