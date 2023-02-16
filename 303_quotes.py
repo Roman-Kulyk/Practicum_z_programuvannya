516861cfe33eada209d2402357b93a77bfb6b09c
Roman Kulyk <roman.kulyk@globallogic.com>
2023-02-16 14:46:30 +0200

    added 303_quotes.py file

---
+++ 303_quotes.py
@@ -0,0 +1,9 @@
+'''Знайдіть відому цитату, яка вам подобається. Збережіть ім’я автора вислову
+у змінній famous_person. Cкладіть повідомлення і збережіть його у новій змінній
+з ім’ям message. Виведіть своє повідомлення. Результат повинен виглядати
+приблизно так (включаючи лапки): Albert Einstein once said, "A person who never
+made a mistake never tried anything new.".'''
+
+message = '"The greatest glory in living lies not in never falling, but in rising every time we fall."'
+famous_person = 'Nelson Mandela'
+print(famous_person + ' once said, ' + message)
