eafed4347624dcd3223a153b5d26536660261011
Roman Kulyk <roman.kulyk@globallogic.com>
2023-02-16 14:38:37 +0200

    added 302_print_name.py file

---
+++ 302_print_name.py
@@ -0,0 +1,7 @@
+'''Збережіть ім’я користувача у змінній і виведіть повідомлення, призначене для
+конкретної людини. Повідомлення повинно бути, наприклад, таким: "Hello, Sasha,
+would you like to learn some Python today?".'''
+
+name = input('Enter your name: ')
+message = "Hello, " + name + ', would you like to learn some Python today?'
+print(message)
