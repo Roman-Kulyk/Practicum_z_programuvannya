dfd3febe304201d7bd1a1e73c0e9fa6905ff0a4d
Roman Kulyk <roman.kulyk@globallogic.com>
2023-02-17 07:13:02 +0200

    added task_25.py file

---
+++ task_25.py
@@ -0,0 +1,7 @@
+#25.Напишіть програму, яка отримує три цифри і друкує їх суму. Кожна цифра
+#вводиться на окремому рядку.
+num_1 = int(input('Enter first number: '))
+num_2 = int(input('Enter second number: '))
+num_3 = int(input('Enter third number: '))
+sum = num_1 + num_2 + num_3
+print(sum)
