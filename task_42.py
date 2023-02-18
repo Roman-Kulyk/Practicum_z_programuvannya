34b25bf3514105a9b14d25e4807023bda37db9f5
Roman Kulyk <roman.kulyk@globallogic.com>
2023-02-18 21:56:20 +0200

    added task_42.py file

---
+++ task_42.py
@@ -0,0 +1,10 @@
+#42.Перевірте приналежність до типу int таких величин як ціле число, текстовий
+#рядок, дробове число.
+
+a = 12
+b = 'Guido van Rossum'
+c = 19.56
+
+print(type(a) is int)
+print(type(b) is int)
+print(type(c) is int)
