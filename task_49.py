46f86551e9d99fbdbd0cc5caf766fa2e33fdd715
Roman Kulyk <roman.kulyk@globallogic.com>
2023-02-19 22:32:59 +0200

    added task_49.py filel

---
+++ task_49.py
@@ -0,0 +1,6 @@
+#49.Напишіть програму, яка отримує від користувача два числа a і b
+#і виводить значення виразу a**b.
+
+a = int(input('Enter a number: '))
+b = int(input('Enter a number: '))
+print(pow(a,b))
