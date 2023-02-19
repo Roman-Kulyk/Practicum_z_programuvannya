f88f6fdd7bc332e2b60f791854d9544d4936aa63
Roman Kulyk <roman.kulyk@globallogic.com>
2023-02-19 22:32:22 +0200

    added task_48.py file

---
+++ task_48.py
@@ -0,0 +1,9 @@
+#48.Сашко, зазвичай, спить вночі a годин і влаштовує собі вдень тиху годину на
+#b хвилин. Визначте, скільки всього хвилин Сашко спить надобу.
+
+a = int(input('Enter a time in hours: '))
+b = int(input('Enter a time in minutes: '))
+
+total = a * 60 + b
+print(total)
+
