69ed5d33ca7a355ceae878106d7e4d66db457480
Roman Kulyk <roman.kulyk@globallogic.com>
2023-03-02 22:34:56 +0200

    deleted task_10-19.py files; added task_90.py file

--- task_11.py
+++
@@ -1,4 +0,0 @@
-#Дано двоцифрове число. Знайдіть число десятків у ньому.
-number=int(input('Enter a number: '))
-tens=number//10
-print(tens)
--- task_14.py
+++
@@ -1,7 +0,0 @@
-#Напишіть програму для запису і розв’язування виразу(a + b) * (a + b), де a і b
-#- натуральні цілі числа.
-
-a=int(input('Enter first number: '))
-b=int(input('Enter second number: '))
-result = (a + b) * (a + b)
-print(f'({a} + {b}) * ({a} + {b}) = {result}')
