9ac51ff8eb9c5442bd3a5b194262bd441f473978
Roman <roman.kulyk@globallogic.com>
2023-03-09 22:31:59 +0200

    added exam option to the special_task.py file

--- special_task.py
+++ special_task.py
@@ -4,11 +4,19 @@
 
 num = float(input('Enter a currency rate: '))
 values = 300
+
 lessons_price = 313 * 30
 print('You have to pay ' + str(lessons_price)+ ' for lessons.')
+
 sum = num * values
 tax = sum * 0.05
 print('You have to left ' + str(tax)+ ' for tax.')
+
 rest = sum - tax - lessons_price
 print('You must pay additional ' + str(rest) + ' for credit in this case, immediately.')
 
+exam_price = num * 250
+print('You have to pay ' + str(exam_price)+ ' for the exam.')
+
+rest_1 = sum - tax - exam_price
+print('You must pay additional ' + str(rest_1) + ' for credit in this case, immediately.')
