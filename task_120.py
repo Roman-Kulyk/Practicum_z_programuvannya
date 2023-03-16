d55116e7b498ab03bd6daabb68c4af6340337de8
Roman <roman.kulyk@globallogic.com>
2023-03-16 22:02:40 +0200

    added task_120.py file

--- task_120.py
+++ task_120.py
@@ -6,6 +6,6 @@ s = int(input('Enter a distance (km): '))
 t = int(input('Enter a time(min): '))
 
 if (s / t) * 60 > 60:
-    print('You did not follow the rule')
+    print('You did not follow the rule.')
 else:
     print('Everything is fine, you can drive further.')
