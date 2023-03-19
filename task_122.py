441c960864cf28738df7af8d42f899fcb7bffe80
Roman <roman.kulyk@globallogic.com>
2023-03-19 20:56:52 +0200

    adde task_122.py not solved

---
+++ task_122.py
@@ -0,0 +1,17 @@
+#122. Дано п’ять цілих чисел a, b, c, d, e. Визначити, скільки з них додатних.
+a=int(input('Enter a number: '))
+b=int(input('Enter a number: '))
+c=int(input('Enter a number: '))
+d=int(input('Enter a number: '))
+e=int(input('Enter a number: '))
+
+
+
+
+for i in range(a,e):
+    i = 0
+    if i > 0:
+        i += 1
+    else:
+        i += 0
+print(i)
