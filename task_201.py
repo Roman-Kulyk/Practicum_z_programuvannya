0a10437311253d7eeb9001eb7318ce4f81cd4ef9
Roman <roman.kulyk@globallogic.com>
2023-04-05 10:13:59 +0300

    fixed output issue with task_201.py file

--- task_201.py
+++ task_201.py
@@ -5,12 +5,16 @@
 
 n = int(input("Enter a number: "))
 if (n % 10) == (1 % 10):                        # 21 31 41 51 61 71 81 91 
-    print("korova")
-elif 2 <= n % 10 <= 4:                          # 22 23 24 32 33 34 42 43 44 
-    print("korovy")
+    print(f"{n} korova")
+
 elif 5 <= n <=20 :                              # 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
-    print("koriv")
+    print(f"{n} koriv")
+
+elif 2 <= n % 10 <= 4:                          # 22 23 24 32 33 34 42 43 44 
+    print(f"{n} korovy")
+
 elif 5 <= (n % 10) <= 9:                        # 25 26 27 28 29  35 36 37 38 38  45 46 47 48 49 
-    print("koriv")
+    print(f"{n} koriv")
+    
 elif n % 10 == 0:                               # 30 40 50 60 70 80 90 
-    print("koriv")
+    print(f"{n} koriv")
