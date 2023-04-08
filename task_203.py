36e082b64065171e2cc4396b64149a43e6dfb675
Roman <roman.kulyk@globallogic.com>
2023-04-08 18:09:02 +0300

    added task_203.py file

--- task_203.py
+++ task_203.py
@@ -1,1 +1,6 @@
-#203. 
+#203. Виведіть повідомлення Hello, Python! на екран n разів (n - ціле число, яке вводить користувач).
+
+
+n = int(input("Enter a number: "))
+for i in range(n):
+    print("Hello, Python!")
