cd2014983dd4b2694a86c639bde8872fb70e1e15
Roman <roman.kulyk@globallogic.com>
2023-04-08 20:19:54 +0300

    added task_206.py and task_210.py not solved

---
+++ task_206.py
@@ -0,0 +1,11 @@
+#206. Користувач вводить кількість навчальних предметів n, а потім, 
+# відповідно, оцінки учня з n навчальних предметів. 
+# Визначте середню оцінку.
+
+
+n = int(input("Enter a nuber of subjects: "))
+for i in range(n):
+    list = []
+    a = int(input("Enter your grade: "))
+    list += a
+    print(list)
