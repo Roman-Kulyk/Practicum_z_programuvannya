424b4f68dfaa8836a5fab677f6381d8b09be1363
Roman <roman.kulyk@globallogic.com>
2023-04-15 21:07:42 +0300

    added task_226_229 not solved, need to calculate sum of list's number

---
+++ task_227.py
@@ -0,0 +1,9 @@
+# 227. При заданому користувачем значенні цілого числа n ≥ 2 обчислити вираз 1 × 2 + 2 × 3 + …​ + (n - 1) × n.
+
+
+n = int(input("Enter a number: "))
+num_list = []
+for i in range(1,n):
+    mul = i *(i + 1)
+    num_list.append(mul)
+print(num_list)
