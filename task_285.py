f898eee74fc032508f7cbac1a9670e76847988d5
Roman <roman.kulyk@globallogic.com>
2023-04-27 09:32:31 +0300

    added task_285.py file

---
+++ task_285.py
@@ -0,0 +1,29 @@
+# 285. Послідовність складається з різних натуральних чисел і завершується числом 0.
+# Визначте значення другого за величиною елемента в цій послідовності. 
+# Вводиться послідовність цілих чисел, що закінчується числом 0 
+# (саме число 0 в послідовність не входить, а використовується як ознака її закінчення). 
+# Гарантується, що в послідовності є хоча б два елементи.
+'''
+Вхідні дані:
+
+1
+4
+3
+2
+0
+
+Вихідні дані:
+
+3
+
+'''
+num_list = []     
+while True:
+    curr = int(input("Enter a number: "))#it reads elements of sequence
+    if curr == 0:#it checks if element not equal to zero
+        break
+    else:
+        num_list.append(curr)# it adds element to the sequence
+        
+            
+print(num_list[2])#it prints quantity of needed elements
