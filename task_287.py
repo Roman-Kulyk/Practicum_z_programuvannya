7de4e9707d081cdb00952e2b361f531cfeacc2e2
Roman <roman.kulyk@globallogic.com>
2023-04-28 12:49:09 +0300

    added comments to the task_287.py file

--- task_287.py
+++ task_287.py
@@ -26,24 +26,24 @@ while True:
     if curr == 0:#it checks if element not equal to zero
         break
     else:
-        sequence.append(curr)
-        prev = sequence[0]
-         
-        if prev > 0:
-            prev_sign = 1
+        sequence.append(curr) #it appends element to the sequence list
+        prev = sequence[0] #it assign first sequence's element to prev variable
+
+        if prev > 0: #it checks if prev variable more than 0
+            prev_sign = 1 #in this case it initialize prev_sign variable with 1
         else:
-            prev_sign = -1
-        sign_changes = 0
+            prev_sign = -1 #in other case it initialize prev_sign variable with -1
+        sign_changes = 0 #it initialiaze sign_changes variable with 0
 
     for  cur in sequence[1:]:
-        if cur > 0:
-            cur_sign = 1
+        if cur > 0: #it checks if cur variable more than 0
+            cur_sign = 1 #in this case it initialize cur_sign variable with 1
         else:
-            cur_sign = -1
+            cur_sign = -1 #in other case it initialize cur_sign variable with -1
 
-        if cur_sign != prev_sign:
-            sign_changes+=1
-        prev = cur
-        prev_sign=cur_sign
+        if cur_sign != prev_sign: #it checks if cur_sign variable is not equal to prev_sign variable
+            sign_changes+=1 #in this case it add 1 to the sign_changes counter
+        prev = cur #it assign cur variable to prev variable
+        prev_sign=cur_sign #it assign cur_sign variable to prev_sign variable
 
-print(sign_changes)
+print(sign_changes) #it prints sign_changes variable
