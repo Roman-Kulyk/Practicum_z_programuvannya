b6597777e190f0f74a85f7a7c32077ceb1043e78
Roman <roman.kulyk@globallogic.com>
2023-05-05 19:18:50 +0300

    added task_305.py file

---
+++ task_305.py
@@ -0,0 +1,15 @@
+# 305.Скласти програму, 
+# яка запитує назву баскетбольної команди і повторює її на екрані зі словами: 
+# This is a champion!.
+'''
+Вхідні дані:
+
+Atlanta Hawks
+
+Вихідні дані:
+
+Atlanta Hawks! This is a champion!
+
+'''
+message = input("Enter a team name: ")
+print(message,'! This is a champion!')
