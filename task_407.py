37f1e77ad1673c8ca8f7bd0d7778f6500787c157
Roman <roman.kulyk@globallogic.com>
2023-05-29 12:26:25 +0300

    added task_407.py file

---
+++ task_407.py
@@ -0,0 +1,19 @@
+# 407. Збережіть назви мов світу, які вводяться в одному рядку через пропуск, у 
+# списку. Простежте за тим, щоб елементи у списку не зберігались в алфавітному 
+# порядку. Відсортуйте список в алфавітному порядку і виведіть його елементи в 
+# рядку через пропуск.
+'''
+Вхідні дані:
+Ukrainian French Bulgarian Norwegian Latvian
+
+Вихідні дані:
+Bulgarian French Latvian Norwegian Ukrainian
+'''
+txt = input("Enter your string: ")
+
+x = txt.split()
+print(x)
+x.sort()
+print(x) 
+
+
