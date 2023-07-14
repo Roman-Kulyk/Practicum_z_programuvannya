a8cd665c95833e6f7c9b3682a0a70e2469140b03
Roman <roman.kulyk@globallogic.com>
2023-07-14 21:05:08 +0300

    added task_608.py filee

---
+++ task_608.py
@@ -0,0 +1,15 @@
+# 608. Напишіть функцію, яка отримує рядок і ціле число n 
+# та повертає n копій заданого рядка.
+'''
+Вхідні дані:
+I love coding
+3
+
+Вихідні дані:
+I love codingI love codingI love coding
+'''
+def i_love(anything):
+    print(anything * 3)
+
+
+i_love('I love coding')
