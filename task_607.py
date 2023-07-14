afcdc181487c6d5b08581dc49ef40d320a64e26d
Roman <roman.kulyk@globallogic.com>
2023-07-14 17:29:54 +0300

    added task_607.py file

---
+++ task_607.py
@@ -0,0 +1,14 @@
+# 607.Напишіть функцію, яка отримує ім’я і друкує вітальне повідомлення.
+'''
+Вхідні дані:
+Alex
+
+Вихідні дані:
+Hello, Alex
+'''
+def greetings(name):
+    print("Hello, ",name)
+
+greetings('Alex')
+greetings('Andrew')
+greetings('to all PEOPLE all around the world!')
