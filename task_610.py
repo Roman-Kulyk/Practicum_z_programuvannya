26a3ee439adf79cd739105d8f34b388400243f1b
Roman <roman.kulyk@globallogic.com>
2023-07-14 21:07:08 +0300

    added task_610.py file

---
+++ task_610.py
@@ -0,0 +1,14 @@
+# 610. Напишіть функцію для перетворення цілого числа в рядок.
+'''
+Вхідні дані:
+8
+
+Вихідні дані:
+'8'
+'''
+def int_to_str(var_1):
+    row = str(var_1)
+    return row
+
+print(int_to_str(8))
+print(type(int_to_str(8)))
