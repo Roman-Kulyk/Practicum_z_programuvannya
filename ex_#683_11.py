f7d81d7561071b75f185c4f6d6ea0c123d641870
Roman <roman.kulyk@globallogic.com>
2023-07-31 22:50:02 +0300

    added ex_#683_11.py file

---
+++ ex_#683_11.py
@@ -0,0 +1,28 @@
+# Створіть програму, яка зчитує позицію шахової фігури від користувача і повідомляє 
+# про колір квадрату, де знаходиться фігура. Наприклад, якщо користувач вводить a і 1, 
+# то програма повинна повідомити, що квадрат має колір black, якщо d і 5 - програма 
+# повідомляє, що квадрат має колір white.
+
+def find_square_color(position):
+    letter = position[0]
+    number = int(position[1])
+    if letter in['a','c','e','g']:
+        if number % 2 != 0:
+            color = 'black'
+        else:
+            color = 'white'
+    elif letter in['b','d','f','h']:
+        if number % 2 != 0:
+            color = 'white'
+        else:
+            color = 'black'
+    return color
+    
+position = input("Enter position: ")
+color = find_square_color(position)
+print(color)
+    
+    
+        
+
+
