07e05bb92c0314d0faffe3b1897e80232fcf5a72
Roman <roman.kulyk@globallogic.com>
2023-08-02 17:35:42 +0300

    added comments to ex_#683_17.py file

--- ex_#683_17.py
+++ ex_#683_17.py
@@ -5,10 +5,12 @@
 # Скористайтеся вказівками з попередньої задачі.
 
 def pass_gen():
+
+
     import random
-    for symbol in range(random.randint(7,10)):
-        symbol = random.randrange(33,126)
-        print(chr(symbol), end ='')
+    for symbol in range(random.randint(7,10)):#it generates different amount of numbers in password
+        symbol = random.randrange(33,126)#it generates different value for every number
+        print(chr(symbol), end ='')#it turns numbers into symbols and prints them
     print()
 
 pass_gen()
