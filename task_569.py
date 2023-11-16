8a317f4be43d46e7a08460c8a8b8d3f672741df6
Roman <roman.kulyk@globallogic.com>
2023-11-16 23:32:23 +0200

    added task_569.py file n/s

---
+++ task_569.py
@@ -0,0 +1,19 @@
+# 569. Дано список значень різних типів даних. Створіть словник із значеннями списку 
+# як ключами, і назвами відповідного типу даних як значеннями словника. У словнику можуть 
+# бути присутні дані одного типу. Надрукуйте вміст словника як у вихідних даних.
+"""
+Вхідні дані:
+Немає
+
+Вихідні дані:
+<class 'int'> 1952 1000000
+<class 'float'> 10.45 5.5
+<class 'complex'> (2+3j)
+<class 'bool'> False
+<class 'str'> pythonguide.pp.ua
+<class 'tuple'> (1, -6)
+<class 'list'> [3, 15]
+<class 'dict'> {'Class C': ['Volkswagen Golf', 'Ford Focus'], 'Class F': ['Audi A8', 'Bentley', 'Maybach'], 'E': ['Toyota Camry']} {}
+<class 'NoneType'> None
+"""
+
